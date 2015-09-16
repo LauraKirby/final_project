ef3ca99ef76b4accad2c0e0c2c16ce3f82ca3c54
LauraKirby <laurakirby26@gmail.com>
2015-09-16 14:17:36 -0700

    fitbit data is stored to current user

--- healthy_me/app/controllers/callbacks_controller.rb
+++ healthy_me/app/controllers/callbacks_controller.rb
@@ -1,16 +1,22 @@
 class CallbacksController < Devise::OmniauthCallbacksController
+  before_action :authenticate_user!, only: [:fitbit]
+
   def facebook
+    # devise creates user
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    binding.pry
-    # is from_omniauth finding the user? if so, where does user creation occur?
+    # you can probably pass a view parameter instead of @user
+    # create a custom after signup path, within controller
     sign_in_and_redirect @user
   end
 
+#how will i refresh the fitbit data each time a user logs in or how often will the data update 
+#does fitbit give me access to what was completed last week?
+#maybe use firebase to update datbases live 
   def fitbit
-  	@user = User.find(:id)
-		@user.fitbit_token = request.env["omniauth.auth"].credentials.token
-  	@user.fitbit_secret = request.env["omniauth.auth"].credentials.secret
-  	binding.pry
+		current_user.fitbit_token = request.env["omniauth.auth"].credentials.token
+  	current_user.fitbit_secret = request.env["omniauth.auth"].credentials.secret
+    binding.pry
+    current_user.save!
   	redirect_to :users
   end 
 end
--- healthy_me/app/controllers/users_controller.rb
+++ healthy_me/app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
+before_action :authenticate_user!
 
 	def index
-
+		#if user is authenticated, render this page
+	
 	end 
 
 end
+
+#add a before action for when user is not authenticated
