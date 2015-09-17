20a3271b57961f6610485a727ce64b7adf5b1fb2
LauraKirby <laurakirby26@gmail.com>
2015-09-16 18:21:29 -0700

    created instance of user, can now use fitgem methods

--- healthy_me/app/controllers/callbacks_controller.rb
+++ healthy_me/app/controllers/callbacks_controller.rb
@@ -9,14 +9,26 @@ class CallbacksController < Devise::OmniauthCallbacksController
     sign_in_and_redirect @user
   end
 
-#how will i refresh the fitbit data each time a user logs in or how often will the data update 
-#does fitbit give me access to what was completed last week?
-#maybe use firebase to update datbases live 
+#how will i refresh the fitbit data each time a user logs in or how often will the data be updated 
+#does fitbit give me access to what was completed last week? yes, you can pass in the date
+#maybe use firebase to update datbases in real time
   def fitbit
-		data = request.env["omniauth.auth"]
+    config = {}
+    data = request.env["omniauth.auth"]
+    config[:oauth] = {consumer_key: ENV["FITBIT_CLIENT_KEY"],
+          consumer_secret: ENV["FITBIT_CLIENT_SECRET"],
+          ssl: true,
+          token: data.credentials.token,
+          secret: data.credentials.secret}
+
+    client = Fitgem::Client.new(config[:oauth])
+
+    client.activities_on_date('today') #works!
+
+
     current_user.fitbit_token = data.credentials.token
   	current_user.fitbit_secret = data.credentials.secret
-    #binding.pry
+   
     current_user.save!
     redirect_to :users
   end 
