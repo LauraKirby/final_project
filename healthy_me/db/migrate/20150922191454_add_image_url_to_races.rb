cf7e99c8435ba0a6724bef693664de42edd460b6
LauraKirby <laurakirby26@gmail.com>
2015-09-22 16:07:44 -0700

    add default images for user image and race image

--- healthy_me/db/migrate/20150922191454_add_image_url_to_races.rb
+++ healthy_me/db/migrate/20150922191454_add_image_url_to_races.rb
@@ -1,5 +1,5 @@
 class AddImageUrlToRaces < ActiveRecord::Migration
   def change
-    add_column :races, :image_url, :string
+    add_column :races, :image_url, :string, default: "placeholder_image.png"
   end
 end
--- healthy_me/db/schema.rb
+++ healthy_me/db/schema.rb
@@ -26,31 +26,31 @@ ActiveRecord::Schema.define(version: 20150922191454) do
     t.string   "map"
     t.string   "elevation"
     t.string   "checkpoints"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-    t.string   "image_url"
+    t.datetime "created_at",                                       null: false
+    t.datetime "updated_at",                                       null: false
+    t.string   "image_url",      default: "placeholder_image.png"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",                       null: false
+    t.string   "encrypted_password",     default: "",                       null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,                        null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                                null: false
+    t.datetime "updated_at",                                                null: false
     t.string   "provider"
     t.string   "uid"
     t.string   "fitbit_token"
     t.string   "fitbit_secret"
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "image_url"
+    t.string   "image_url",              default: "default_user_image.png"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
