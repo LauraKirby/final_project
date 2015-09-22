f959626bf7018e9e9c26c1f0fd3490bc1caa6fed
LauraKirby <laurakirby26@gmail.com>
2015-09-22 14:34:40 -0700

    add bay to breakers json data, use json & seed file to insert data into the database

--- healthy_me/db/seeds.rb
+++ healthy_me/db/seeds.rb
@@ -2,15 +2,21 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 #when adding a user with devise you must add email, password & password_confirmation
+
+file = File.read('data-bay-to-breakers.json')
+b2b_hash = JSON.parse(file)
+
 u1 = User.create(first_name: "Thomas", last_name: "The Train", password:"thomas@example.com", password_confirmation: "thomas@example.com", email:"thomas@example.com", image_url: "http://bit.ly/1LLPIIr")
 
 u2 = User.create(first_name: "Olivia", last_name: "The Pig", password:"olivia@example.com", password_confirmation: "olivia@example.com", email: "olivia@example.com", image_url:"http://bit.ly/1LLPIIr")
 
-r1 = Race.create(name: "Bay to Breakers", distance: "7 miles", image_url:"http://bit.ly/1OsU11r", date: "05/15/2016", cost: 59)
+r1 = Race.create(name: b2b_hash["name"], distance: b2b_hash["distance"], image_url:b2b_hash["image"], date: b2b_hash["date"], cost: b2b_hash["cost"])
 
 r2 = Race.create(name: "Boston Marathon", distance: "26 miles", image_url:"http://bit.ly/1OOXsyT", date: "04/15/2016", cost: 175)
 
 u1.races << r1
+
+u2.races << r2
 
 #use ruby's open method to access json file
 #then parseJSON
