d299b17e437b26ca5a6566e3a938f778f269bf8a
LauraKirby <laurakirby26@gmail.com>
2015-09-24 21:53:41 -0700

    write to-do for tomorrow

--- app/assets/javascripts/angular/controllers.js
+++ app/assets/javascripts/angular/controllers.js
@@ -1,5 +1,7 @@
 app.controller("UsersController", ["$scope", "$http", function($scope, $http) {
 	$scope.testing = "testing";
+    //TO DO 
+    //set up envServiceProvider
 	$http.get('http://localhost:3000/users/data.json').then(function(data){
     	$scope.user_data = data.data;
     	$scope.user_email = $scope.user_data.email;
