a06c11b9e59ab2265687e20c7d0ac1401f9a87cc
LauraKirby <laurakirby26@gmail.com>
2015-09-24 20:11:11 -0700

    remove http

--- app/assets/javascripts/angular/controllers.js
+++ app/assets/javascripts/angular/controllers.js
@@ -1,6 +1,6 @@
 app.controller("UsersController", ["$scope", "$http", function($scope, $http) {
 	$scope.testing = "testing";
-	$http.get('http://localhost:3000/users/data.json').then(function(data){
+	$http.get('https://healthyme.herokuapp.com/users/data.json').then(function(data){
     	$scope.user_data = data.data;
     	$scope.user_email = $scope.user_data.email;
     	$scope.user_name = $scope.user_data.first_name; 
