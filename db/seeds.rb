91fbcb4d02dd42763f04b7f0a41af0f96079ac95
LauraKirby <laurakirby26@gmail.com>
2015-10-05 23:52:07 -0700

    add about url back to seed file for ios

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,14 +16,13 @@ b2b_plan_file = File.read('data-plan-steps.json')
 b2b_step = JSON.parse(b2b_plan_file)
 
 #create two races
-b2b = Race.create(name: races_hash["races"][0]["name"], distance: races_hash["races"][0]["distance"], image_url:races_hash["races"][0]["image"], date: races_hash["races"][0]["date"], cost: races_hash["races"][0]["cost"], about:races_hash["races"][0]["about"])
-boston = Race.create(name: races_hash["races"][1]["name"], distance: races_hash["races"][1]["distance"], image_url:races_hash["races"][1]["image"], date: races_hash["races"][1]["date"], cost: races_hash["races"][1]["cost"], about:races_hash["races"][1]["about"])
+b2b = Race.create(name: races_hash["races"][0]["name"], distance: races_hash["races"][0]["distance"], image_url:races_hash["races"][0]["image"], date: races_hash["races"][0]["date"], cost: races_hash["races"][0]["cost"], about:races_hash["races"][0]["about"], about_url:races_hash["races"][0]["about_url"])
+boston = Race.create(name: races_hash["races"][1]["name"], distance: races_hash["races"][1]["distance"], image_url:races_hash["races"][1]["image"], date: races_hash["races"][1]["date"], cost: races_hash["races"][1]["cost"], about:races_hash["races"][1]["about"], about_url:races_hash["races"][1]["about_url"])
 
 #create a fitness plan
 b2b_plan = FitnessPlan.create(plan_title: "Bay To Breakers", plan_summary: "Preparing for a 15k is fun! We have outlined a few steps that will help you prepare for race day")
 
 #create two steps
-
 b2b_day_1 = Step.create(title: b2b_step["two_weeks_before"][1]["focus"], summary: b2b_step["two_weeks_before"][0]["activity"])
 b2b_day_2 = Step.create(title: b2b_step["two_weeks_before"][2]["focus"], summary: b2b_step["two_weeks_before"][1]["activity"])
 b2b_day_3 = Step.create(title: b2b_step["two_weeks_before"][3]["focus"], summary: b2b_step["two_weeks_before"][2]["activity"])
