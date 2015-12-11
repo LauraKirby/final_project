592a1376bbe329b199497dd474c2cbed08fe843f
LauraKirby <laurakirby26@gmail.com>
2015-12-11 15:11:33 -0800

    add promise to implementation of service within controll, service is not working yet

--- app/assets/javascripts/angular/controllers.js
+++ app/assets/javascripts/angular/controllers.js
@@ -1,31 +1,10 @@
 app.controller("UsersController", ["$scope", "$http", "$location", "userService", function($scope, $http, $location, userService) {
-    //TO DO 
-    //set up envServiceProvider
-    //console.log($location.host());
     
-    $scope.user = userService.getUser();
-    console.log($scope.user);
+    userService.getUser().then(function(user_data){
+        $scope.user = user_data;
+        console.log($scope.user);
+    });
 
-    //Make code below a service:
-    //get data from '/users/data.json' 
-    //data for users/angular
-    // $http.get('/users/data.json').then(function(data){
-    // 	//added returnedData as a property to user_data to have the data returned stored an untampered with object.
-    //     $scope.user_data = {};
-    //     $scope.user_data = data.data; 
-    //     //do not need to write out all of these properties on user_data because they already exist, 
-    //     //however, it will make my code more readable
-    //     //Go back to users_controller, to modify the current_user properties that are sent to the client
-    //     $scope.user_data = {
-    //         email: $scope.user_data.email,
-    //         first_name: $scope.user_data.first_name,
-    //         last_name: $scope.user_data.last_name,
-    //         image_url: $scope.user_data.image_url,
-    //         races: $scope.user_data.races,
-    //         current_race: $scope.user_data.current_race
-    //     };
-    //     console.log($scope.user_data.current_race);
-    // });
 }]);
 
 app.controller("RaceController", ["$scope", "$http", function($scope, $http) {
@@ -54,7 +33,6 @@ app.controller("RaceController", ["$scope", "$http", function($scope, $http) {
         $http.post('/races', data).then(function(response){
             $scope.status = response.status;
             $scope.current_race = parseInt(response.data.id) - 1;
-            // console.log(response.data);
          });
     };
 }]);
@@ -65,7 +43,6 @@ app.controller("PlanController", ["$scope", "$http", function($scope, $http) {
         $scope.focus = $scope.steps_data[0].focus;
         $scope.activity = $scope.steps_data[0].activity;
         $scope.duration = $scope.steps_data[0].duration;
-        // console.log($scope.activityOne);
     });
     $http.get('users/plansummary.json').then(function(data){
         $scope.planSummary = data.data.plan_summary;
@@ -73,3 +50,6 @@ app.controller("PlanController", ["$scope", "$http", function($scope, $http) {
     });
 }]);
 
+//TO DO 
+//set up envServiceProvider
+//console.log($location.host());
--- app/assets/javascripts/angular/services.js
+++ app/assets/javascripts/angular/services.js
@@ -1,23 +1,26 @@
 app.service("userService", function($http) {
+  //returns a promise
   return {
   	getUser: function(){
   		$http.get('/users/data.json').then(function(data){
     	//added returnedData as a property to user_data to have the data returned stored an untampered with object.
-        this.user_data = {};
-        this.user_data = data.data; 
+        user_data = {};
+        user_data = data.data; 
         //do not need to write out all of these properties on user_data because they already exist, 
         //however, it will make my code more readable
         //Go back to users_controller, to modify the current_user properties that are sent to the client
-        this.user_data = {
-            email: this.user_data.email,
-            first_name: this.user_data.first_name,
-            last_name: this.user_data.last_name,
-            image_url: this.user_data.image_url,
-            races: this.user_data.races,
-            current_race: this.user_data.current_race
-        };
-        console.log(this.user_data.email);
-        return this.user_data;
+        //make a comment with all of the properties 
+        // user_data = {
+        //     email: user_data.email,
+        //     first_name: user_data.first_name,
+        //     last_name: user_data.last_name,
+        //     image_url: user_data.image_url,
+        //     races: user_data.races,
+        //     current_race: user_data.current_race
+        // };
+
+        return user_data;
+        
     });
    }
   };
@@ -30,3 +33,8 @@ app.service("userService", function($http) {
 //     {update: {method: "PUT"}} 
 //   );
 // }]);
+
+//Notes
+//.then is a method on a promise 
+//q on github, see tutorials
+//in a promise you can return data or another promise
