c0ef8d5f0113922c0c4758ee211d8b33740e6efc
LauraKirby <laurakirby26@gmail.com>
2015-12-22 15:24:24 -0800

    update controller to match route, remove method which has been replaced, remove html option for races route

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -3,38 +3,17 @@ class RacesController < ApplicationController
 	def index 
 		races_json = File.read('data-races.json')
 		@races = Race.all
-			respond_to do |format|
-				format.json {render json:races_json}
-				format.html {render html:current_user, :partial => "races/index.html.erb"}
-			end 
+		render json: races_json
 	end 
 
-	#change to something like add_user_race
-	def create
+	def add_race_to_user
 		add_race = Race.find_by(name:race_params[:name])
 		current_user.races << add_race
+		# it appears that current_race is not storing this value
+		# after this method, the user.current_race = 0 again
 		current_user.current_race = add_race.id
 		render json: add_race
 	end 
-
-	#to-do refactor to find race by id through route params
-	def about_b2b
-		b2b = Race.first
-		about = b2b.about
-		render json: about
-			# respond_to do |format|
-			# 	format.json {render json:about}
-			# 	format.html {render html:current_user, :partial => "races/index.html.erb"}
-			# end
-	end
-
-	def about_boston
-		boston = Race.last
-		about = boston.about
-		respond_to do |format|
-			format.json {render json:about}
-		end
-	end
 
 	def edit
 		#if race id doesn't exist in array, then push race id
